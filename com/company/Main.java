f92a612e9c1243d70aa07d744949db4bfb967fbd
Alex White <alexlloydwhite@gmail.com>
2021-09-04 14:38:50 -0500

    added casting and strings

--- com/company/Main.java
+++ com/company/Main.java
@@ -12,6 +12,7 @@ public class Main {
         float price = 10.99F;
         char letter = 'A';
         boolean isEligible = true;
+
         // Reference Types
         Date now = new Date();
         System.out.println(now);
@@ -19,6 +20,33 @@ public class Main {
         Point point2 = point1;
         point1.x = 2;
         System.out.println(point2);
+
+        // String Literals
+        String message = "Hello World" + "!!";
+        System.out.println(message);
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.length());
+        System.out.println(message.indexOf("H"));
+        System.out.println(message.indexOf("e"));
+        System.out.println(message.indexOf("sky"));
+        System.out.println(message.replace("!", "*"));
+        // In Java, strings are immutable, so any method which alters the string will return a new string object
+        System.out.println(message);
+
+        // Casting
+        short x = 1;
+        int y = x + 2;
+        System.out.println(y);
+        // Implicit casting AKA automatic conversion
+        // byte > short > int > long > float > double
+        // Casting float points
+        double a = 1.1;
+        double b = a + 2;
+        System.out.println(b);
+        String i = "1";
+        int j = Integer.parseInt(i) + 2;
+        System.out.println(j);
+
     }
 
 }
