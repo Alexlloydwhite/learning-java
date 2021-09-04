0cca3ce875a44fb55f9b57236fb9b1b6a0c8e7f9
Alex White <alexlloydwhite@gmail.com>
2021-09-04 14:45:25 -0500

    adding Math

--- com/company/Main.java
+++ com/company/Main.java
@@ -47,6 +47,19 @@ public class Main {
         int j = Integer.parseInt(i) + 2;
         System.out.println(j);
 
+        // Math
+        int result = Math.round(1.1F);
+        System.out.println(result);
+        int result2 = (int)Math.ceil(1.1F);
+        System.out.println(result2);
+        int result3 = (int)Math.floor((1.1F));
+        System.out.println(result3);
+        int result4 = Math.max(1, 2);
+        System.out.println(result4);
+        int result5 = (int) Math.round(Math.random() * 100);
+        System.out.println(result5);
+        int result6 = (int) (Math.random() * 10000);
+        System.out.println(result6);
     }
 
 }
