ce67695e2fbf23376a7db3afda9a68cb2f9db074
Alex White <alexlloydwhite@gmail.com>
2021-09-23 11:58:19 -0500

    adding constructors

---
+++ com/Construction/Main.java
@@ -0,0 +1,12 @@
+package com.Construction;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Shirt s = new Shirt("White", 'M');
+
+        System.out.println(s.color);
+        System.out.println(s.size);
+    }
+
+}
--- com/Construction/Shirt.java
+++ com/Construction/Shirt.java
@@ -1,4 +1,31 @@
 package com.Construction;
 
 public class Shirt {
+
+    public static String color;
+    public static char size;
+
+    Shirt() {}
+
+    Shirt(String newColor, char newSize) {
+        color = newColor;
+        size = newSize;
+    }
+
+    public static void putOn() {
+        System.out.println("Shirt is on!");
+    }
+
+    public static void takeOff() {
+        System.out.println("Shirt is off!");
+    }
+
+    public static void setColor(String newColor) {
+        color = newColor;
+    }
+
+    public static void setSize(char newSize) {
+        size = newSize;
+    }
+
 }
