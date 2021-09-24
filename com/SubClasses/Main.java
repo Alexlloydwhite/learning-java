501a8a50ab7ee30cacaea7e6a46930722578b13f
Alex White <alexlloydwhite@gmail.com>
2021-09-24 11:29:07 -0500

    created 3 pairs of shoes in the main method

--- com/SubClasses/Main.java
+++ com/SubClasses/Main.java
@@ -3,6 +3,8 @@ package com.SubClasses;
 public class Main {
 
     public static void main (String[] args) {
-
+        Running r = new Running("Nike", 43.0, 250);
+        Walking w = new Walking("Hanwag", 43.5, true);
+        Walking w2 = new Walking("Merrel", 42.0, false);
     }
 }
